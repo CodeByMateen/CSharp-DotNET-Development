d2007faae4dd4bc169d187edcb81b89bf9592a29
Mateen Shahzad <142880053+CodeByMateen@users.noreply.github.com>
2023-12-29 10:59:13 +0500

    Delete UMT Student Course Registration System directory

--- UMT Student Course Registration System/Queries/viewOfferedCourses.sql
+++
@@ -1,3 +0,0 @@
-﻿SELECT Course.CourseID, Section.SecID, Course.CourseName , Section.SecName , Course.CreditHrs , Section.Strength , Faculty.FacName
-FROM Course , Section , Faculty
-WHERE Course.CourseID = Section.CourseID AND Section.FacID = Faculty.FacID;
